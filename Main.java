5fa445f75267f0cfe3703a23843634b232213ac1
Wallace Henrique <144370869+wollihenrique@users.noreply.github.com>
2026-08-08 20:10:28 +0000

    primerio commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       int idade = 20;
+       double altura = 1.75;
+       char letra = 'A';
+       boolean matriculado = true;
+    }
+}
