cdefb4f33b4baf0149da7f28b96f783b9c566252
Wallace Henrique <144370869+wollihenrique@users.noreply.github.com>
2026-08-15 18:15:41 +0000

    lista-de-exercícios-ex06

--- Main.java
+++ Main.java
@@ -1,15 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-       String nome = "Wallace";
+       String nome = "Mikael";
        int idade = 18;
-       double altura = 1.79;
-       float nota = 10.0f;
-       char situacao_matricula = 'A'; //A => Ativa
 
-       System.out.println("Nome: " + nome);
-       System.out.println("Idade: " + idade);
-       System.out.println("Altura: " + altura);
-       System.out.println("Nota: " + nota);
-       System.out.println("Situação da matrícula: " + situacao_matricula);
+       System.out.println("Meu nome é " + nome + ", e tenho " + idade + " anos");
     }
 }
