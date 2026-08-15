417b9bc7a1f1537c23e8e5fb821c5e5680871cbf
Wallace Henrique <144370869+wollihenrique@users.noreply.github.com>
2026-08-15 20:05:13 +0000

    lista-de-exercícios-ex15

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-       float Bombril = 3.50f, dinheiro = 10.0f;
+       float distancia_perc = 79, quantidade_combust = 4.2f, media = distancia_perc / quantidade_combust;
 
-       System.out.println("Produto Bombril: " + Bombril + " R$");
-       System.out.println("Dinheiro pago: " + dinheiro + " R$");
-       System.out.println("Troco: " + (dinheiro - Bombril) + " R$");
+       System.out.println("Distância Percorrida: " + distancia_perc);
+       System.out.println("Combustível gasto: " + quantidade_combust);
+       System.out.println("O veículo faz aproximadamente " + media + " Km/l");
     }
 }
