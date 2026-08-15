ce42bfbad86c16de18d7049655d2b74b3adc773c
Wallace Henrique <144370869+wollihenrique@users.noreply.github.com>
2026-08-15 19:19:27 +0000

    lista-de-exercícios-ex09

--- Main.java
+++ Main.java
@@ -1,12 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-       int num1 = 18, num2 = 35;
+       float nota1 = 8.5f, nota2 = 9.0f, nota3 = 10;
+       float media_aritmetica = (nota1 + nota2 + nota3) / 3;
 
-        System.out.println("Números usados: " + num1 + " e " + num2);
-        System.out.println("Soma: " + (num1 + num2));
-        System.out.println("Subatração: " + (num1 - num2));
-        System.out.println("Multiplicação: " + (num1 * num2));
-        System.out.println("Divisão: " + (num1 / num2));
-        System.out.println("Resto da divisão: " + (num1 % num2));
+        System.out.println("Média aritmética: " + media_aritmetica);
     }
 }
