860ceaa5107823a1efe08fecff5e2ba2f014e642
Wallace Henrique <144370869+wollihenrique@users.noreply.github.com>
2026-08-15 19:13:03 +0000

    lista-de-exercícios-ex08

--- Main.java
+++ Main.java
@@ -1,15 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-       int A = 10, B = 20, C = 0;
+       int num1 = 18, num2 = 35;
 
-       System.out.println("A = " + A);
-       System.out.println("B = " + B);
-
-       C = A;
-       A = B;
-       B = C;
-
-       System.out.println("A = " + A);
-       System.out.println("B = " + B);
+        System.out.println("Números usados: " + num1 + " e " + num2);
+        System.out.println("Soma: " + (num1 + num2));
+        System.out.println("Subatração: " + (num1 - num2));
+        System.out.println("Multiplicação: " + (num1 * num2));
+        System.out.println("Divisão: " + (num1 / num2));
+        System.out.println("Resto da divisão: " + (num1 % num2));
     }
 }
