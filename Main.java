5f84d13be7ebf9a405988cf8eb58e63e45ea1f85
Wallace Henrique <144370869+wollihenrique@users.noreply.github.com>
2026-08-15 17:38:35 +0000

    lista-de-exercícios-ex02

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-       System.out.print("Meu nome é Wallace Henrique B. Santos, sou do curso de Desenvolvimento de Software Multiplataforma.");
+       System.out.println("Mas que nada, sai da minha frente que eu quero passar");
+       System.out.println("Pois o samba está animado e eu quero é sambar");
+       System.out.println("Este samba que é um misto de maracatú");
+       System.out.println("É samba de preto velho, samba de preto tu");
     }
 }
