91834164c8bee3170cbbf8c1bf349faaea681f60
Wallace Henrique <144370869+wollihenrique@users.noreply.github.com>
2026-08-15 19:49:58 +0000

    lista-de-exercícios-ex13

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-       float temperaturaC = 29;
-       float temperaturaF = temperaturaC * 9 / 5 + 32;
+       int segundos_totais = 45087;
+       int horas = segundos_totais / 3600;
+       int resto = segundos_totais % 3600, minutos = resto / 60, segundos = resto % 60;
 
-       System.out.println("Temperatura em Celsius :" + temperaturaC);
-       System.out.println("Temperatura em Fahrenheint :" + temperaturaF);
+
+       System.out.println(segundos_totais + " segundos são " + horas + " horas " + minutos + " minutos " + segundos + " segundos");
     }
 }
