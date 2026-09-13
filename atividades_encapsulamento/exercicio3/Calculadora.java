8f64a7da87ad650bebdaae1246606195fa1afa24
wollihenrique <wallacekada@gmail.com>
2026-09-12 21:49:10 -0300

    feat: adiciona exercícios de polimorfismo

---
+++ atividades_encapsulamento/exercicio3/Calculadora.java
@@ -0,0 +1,13 @@
+public class Calculadora {
+    public int somar (int a, int b) {
+        return a + b;
+    }
+
+    public double somar (double a, double b) {
+        return a + b;
+    }
+
+    public int somar (int a, int b, int c) {
+        return a + b + c;
+    }
+}
---
+++ atividades_encapsulamento/exercicio3/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        System.out.println(calc.somar(20, 10));
+        System.out.println(calc.somar( 2.5, 3.8));
+        System.out.println(calc.somar(1, 2, 3));
+    }
+}
